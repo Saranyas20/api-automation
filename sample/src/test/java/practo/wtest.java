94d0cdd1e834132d1e7c8ce93ccf831d47cc5481
Saranyas20 <Saranyarakshan20@gmail.com>
2024-07-02 10:19:59 +0530

    Update wtest.java

--- sample/src/test/java/practo/wtest.java
+++ sample/src/test/java/practo/wtest.java
@@ -1,6 +1,6 @@
 package practo;
 
-import io.restassured.RestAssured;
+
 import io.restassured.response.Response;
 import org.hamcrest.Matchers;
 import org.testng.annotations.Test;
@@ -15,19 +15,15 @@ public class wtest {
     @Test
     public void testWeatherInLondon() {
         // Set the base URI for RestAssured
-        RestAssured.baseURI = " https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=439d4b804bc8187953eb36d2a8c26a02";
+        String url = "https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=439d4b804bc8187953eb36d2a8c26a02";
 
         // Send the GET request and get the response
-        Response response = given()
-                .queryParam("q", "London,uk")
-                .queryParam("appid", "439d4b804bc8187953eb36d2a8c26a02")
-                .get("/data/2.5/weather?q=London,uk&appid=439d4b804bc8187953eb36d2a8c26a02");
-
+        Response response = given().when().get(url);
         // Assert that the status code is 200
         response.then().statusCode(200);
 
         // Validate the data in the response
-        response.then().body("country", Matchers.equalTo("GB"));
+        response.then().body("base", Matchers.equalTo("stations"));
         response.then().body("name", Matchers.equalTo("London"));
 
         // Validate JSON schema
